6fae4c03c3bd1f541e91d10ce899e894053e0dce
Nguyễn Văn Anh <anh9701zt@gmail.com>
2026-03-03 17:00:37 +0700

    Khoi tao

---
+++ src/main/java/com/example/backend_tmdt/BackendTmdtApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend_tmdt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTmdtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendTmdtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backend_tmdt/BackendTmdtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend_tmdt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTmdtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
